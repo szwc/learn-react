8ad7623d152910a6a1277b27fcfac18d1809614f
szwc <m02@163.com>
2021-06-04 15:01:43 +0800

    init

---
+++ src/views/test.jsx
@@ -0,0 +1,8 @@
+import ReactDom from 'react-dom'
+const element = <h1>hello world!</h1>
+
+const testCom = ReactDom.render(
+    element,
+    document.getElementById('root')
+)
+// export default testCom
