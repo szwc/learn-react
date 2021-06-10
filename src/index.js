6164be7e196785371c19edd4d5eaddec0869018d
LAPTOP-3BGGHIVT\szwc <13718507084@163.com>
2021-06-10 14:27:24 +0800

    react-router v5.x init and demo

--- src/index.js
+++ src/index.js
@@ -1,159 +1,138 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import './index.css'
+// 不适应路由
 
-// class Square extends React.Component {
+import React from 'react'
+// import {render} from 'react-dom'
+import ReactDom from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useRouteMatch,
+  useParams
+} from 'react-router-dom'
+
+class About extends React.Component {
+  // constructor(props){
+  //   super(props)
+  // }
+  render(){
+    return <h1>About page</h1>
+  }
+}
+// class Inbox extends React.Component {
+//   // constructor(props){
+//   //   super(props)
+//   // }
 //   render(){
-//     return (
-//       <button 
-//         className="square" 
-//         onClick={()=>this.props.onClick()}
-//       >
-//         {this.props.value}
-//       </button>
-//     )
+//     return <h1>Inbox page</h1>
 //   }
 // }
-
-// 函数组件
-function Square(props) {
-  return (
-    <button 
-      className="square" 
-      onClick={()=>props.onClick()}
-    >
-      {props.value}
-    </button>
-  )
-}
-
-class Board extends React.Component {
-  
-  renderSquare(i){
-    return (
-      <Square 
-        value={this.props.squares[i]}
-        onClick = {() => this.props.onClick(i)}
-      />
-    )
-  }
-
+class Home extends React.Component {
+  // constructor(props){
+  //   super(props)
+  // }
   render(){
+    return <h1>Home page</h1>
+  }
+}
+function Users() {
+  return <h2>Users</h2>;
+}
 
-    return(
+// 嵌套路由
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name:'app com'
+    }
+  }
+  render(){
+    return (
+      <Router>
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
+    </Router>
     )
   }
-}
-
-class Game extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      history:[{
-        squares:Array(9).fill(null),
-      }],
-      stepNumber:0,
-      xIsNext:true
-    }
-  }
-  handleClick(i){
-    const history = this.state.history.slice(0,this.state.stepNumber + 1)
-    const current = history[history.length - 1]
-    const squares = current.squares.slice()
-    if(calculateWinner(squares) || squares[i]){
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X' :'O'
-    this.setState({
-      history:history.concat([{
-        squares:squares
-      }]),
-      stepNumber:history.length,
-      xIsNext:!this.state.xIsNext
-    })
-  }
-  jumpTo(step){
-    this.setState({
-      stepNumber:step,
-      xIsNext:(step % 2) === 0
-    })
-  }
-  render() {
-    const history = this.state.history
-    const current = history[this.state.stepNumber]
-    const winner = calculateWinner(current.squares)
-
-    const moves = history.map((step,move) =>{
-      const desc = move ? 
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}> 
-          <button onClick={()=>this.jumpTo(move)}>{desc}</button>
-        </li>
-      )
-    })
-
-    let status
-    if(winner){
-      status  = 'Winner: ' + winner 
-    }else{
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' :'O')
-    }
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares = {current.squares}
-            onClick = {(i)=>this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+  
 }
 ReactDom.render(
-  <Game />,
+  <App/>,
   document.getElementById('root')
 )
+
+
